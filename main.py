e308c0ecf2bb82228801ddeb6e5a381251442f26
catsara12 <nicoledelgado1012@gmail.com>
2025-05-07 01:05:35 +0000

    fix: usar token desde variable de entorno

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import os
 from telegram.ext import ApplicationBuilder, ChatJoinRequestHandler, ContextTypes
 
-TOKEN = "7297046332:AAFpi4TDuTfoZYEAZdbeHnOXXE1qdgDfHVk
-
-"
+# Leer el token desde la variable de entorno
+TOKEN = os.environ["TOKEN"]
 
 async def auto_approve(update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        await context.bot.approve_chat_join_request(update.chat_join_request.chat.id, update.chat_join_request.from_user.id)
+        await context.bot.approve_chat_join_request(
+            chat_id=update.chat_join_request.chat.id,
+            user_id=update.chat_join_request.from_user.id
+        )
         print(f"Aprobado: {update.chat_join_request.from_user.username}")
     except Exception as e:
         print(f"Error al aprobar solicitud: {e}")
 
+# Crear la app del bot y agregar el manejador de aprobaciones
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(ChatJoinRequestHandler(auto_approve))
+
 print("Bot iniciado correctamente. Esperando solicitudes...")
 app.run_polling()
